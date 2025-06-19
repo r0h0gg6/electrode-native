7378f34fbbca9dc523b37ba3571dd33be4c99853
r0h0gg6 <richard.heng0@walmart.com>
2025-06-18 17:03:24 -0700

    Update unit tests and fixtures for RN 0.77

--- ern-runner-gen-android/test/AndroidRunnerGenerator-test.ts
+++ ern-runner-gen-android/test/AndroidRunnerGenerator-test.ts
@@ -70,6 +70,30 @@ describe('Test AndroidRunnerGenerator for RN version 0.72.x', () => {
     targetPlatform: 'android',
   };
 
+  describe('Test AndroidRunnerGenerator for RN version 0.77.x', () => {
+  const fixtureRunnerPath = path.join(
+    __dirname,
+    'fixtures',
+    'simple-android-runner-rn-77',
+  );
+  const generatedRunnerPath = tmp.dirSync({ unsafeCleanup: true }).name;
+  process.chdir(generatedRunnerPath);
+
+  const generatorConfig: RunnerGeneratorConfig = {
+    extra: {
+      androidConfig: {
+        artifactId: 'runner-ern-container-dummy',
+        groupId: 'com.walmartlabs.ern',
+        packageFilePath: 'com/walmartlabs/ern/dummy',
+        packageName: 'com.walmartlabs.ern.dummy',
+      },
+    },
+    mainMiniAppName: 'dummy',
+    outDir: generatedRunnerPath,
+    reactNativeVersion: '0.77.0',
+    targetPlatform: 'android',
+  };
+
   it('should generate simple-android-runner fixture given same configuration', async () => {
     await new AndroidRunnerGenerator().generate(generatorConfig);
     assert(
